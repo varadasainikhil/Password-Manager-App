5e326ebcffeb51c3109de381e69900fd3078ca92
Sai Nikhil Varada <varadasainikhil@gmail.com>
2023-04-06 17:17:26 +0530

    Complete working app, with working password generator.

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
 from tkinter import *
 from tkinter import messagebox
+import random
+
 FONT_NAME = "Courier"
 
-
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = [random.choice(letters) for char in range(nr_letters)]
+    password_list += [random.choice(symbols) for char in range(nr_symbols)]
+    password_list += [random.choice(numbers) for char in range(nr_numbers)]
+
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+
+    password_entry.delete(0, END)
+    password_entry.insert(0, password)
+
+
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
@@ -11,7 +38,7 @@ def save_password():
     email_text = email_entry.get()
     password_text = password_entry.get()
 
-    if len(website_text) == 0 or len(password_text) == 0:
+    if len(website_text) == 0 or len(password_text) == 0 or len(email_text) == 0:
         messagebox.showinfo(title="Error", message="Please fill out all the fields.")
     else:
         is_ok_to_save = messagebox.askokcancel(title="Confirm?",
@@ -22,7 +49,6 @@ def save_password():
                 file.write(f"{website_text} | {email_text} | {password_text} \n")
             website_entry.delete(0, END)
             password_entry.delete(0, END)
-
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -56,7 +82,7 @@ password_label.grid(row=3, column=0)
 password_entry = Entry(width=34)
 password_entry.grid(row=3, column=1)
 
-generate_password_button = Button(text="Generate Password", width=14, padx=0, pady=0)
+generate_password_button = Button(text="Generate Password", width=14, padx=0, pady=0, command=generate_password)
 generate_password_button.grid(row=3, column=2)
 
 add_button = Button(text="Add", width=44, command=save_password)
