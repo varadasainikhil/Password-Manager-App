8bcbd458ae150fee752bfe7e4aa4623114bae65a
Sai Nikhil Varada <varadasainikhil@gmail.com>
2023-04-06 16:44:13 +0530

    Working UI and saving password and check for empty fields implemented

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+from tkinter import messagebox
 FONT_NAME = "Courier"
 
 
@@ -7,10 +7,22 @@ FONT_NAME = "Courier"
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
-    with open(file="passwords.txt", mode="a") as file:
-        file.write(f"{website_text.get()} | {email_text.get()} | {password_text.get()} \n")
-    website_text.delete(0, END)
-    password_text.delete(0, END)
+    website_text = website_entry.get()
+    email_text = email_entry.get()
+    password_text = password_entry.get()
+
+    if len(website_text) == 0 or len(password_text) == 0:
+        messagebox.showinfo(title="Error", message="Please fill out all the fields.")
+    else:
+        is_ok_to_save = messagebox.askokcancel(title="Confirm?",
+                                               message=f"The details entered as :\n Website : {website_text} \n "
+                                                       f"Email : {email_text} \n Password : {password_text}")
+        if is_ok_to_save:
+            with open(file="passwords.txt", mode="a") as file:
+                file.write(f"{website_text} | {email_text} | {password_text} \n")
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -27,22 +39,22 @@ canvas.grid(row=0, column=1)
 website_label = Label(text="Website", font=(FONT_NAME, 12, "bold"))
 website_label.grid(row=1, column=0)
 
-website_text = Entry(width=52)
-website_text.grid(row=1, column=1, columnspan=2)
-website_text.focus()
+website_entry = Entry(width=52)
+website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
 
 email_label = Label(text="Email/Username", font=(FONT_NAME, 12, "bold"))
 email_label.grid(row=2, column=0)
 
-email_text = Entry(width=52)
-email_text.grid(row=2, column=1, columnspan=2)
-email_text.insert(0, "varadasainikhil@gmail.com")
+email_entry = Entry(width=52)
+email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "varadasainikhil@gmail.com")
 
 password_label = Label(text="Password", font=(FONT_NAME, 12, "bold"))
 password_label.grid(row=3, column=0)
 
-password_text = Entry(width=34)
-password_text.grid(row=3, column=1)
+password_entry = Entry(width=34)
+password_entry.grid(row=3, column=1)
 
 generate_password_button = Button(text="Generate Password", width=14, padx=0, pady=0)
 generate_password_button.grid(row=3, column=2)
